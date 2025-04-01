36b7451ac2651d8abf107542b1a422c17d962a31
Pinn <lsol1130@naver.com>
2025-04-02 00:43:34 +0900

    [fix] 헤더 폰트 수정

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="fixed top-0 left-0 w-full backdrop-blur z-50">
       <div className="mx-auto px-6 py-4 flex justify-between items-center">
-        <h1 className="ml-10 font-eliceBold text-7xl text-gray-700">Pinn .</h1>
+        <h1 className="ml-10 font-hambakk text-7xl text-gray-700">Pinn .</h1>
         <nav className="space-x-6">
           <a href="#about" className="text-gray-600 hover:text-black transition text-2xl">About</a>
           <a href="#skills" className="text-gray-600 hover:text-black transition text-2xl">Skills</a>

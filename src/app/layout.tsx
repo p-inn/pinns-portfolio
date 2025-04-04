b6b4aeb5927042db4cf3e5643dc3d52711cf2158
Pinn <lsol1130@naver.com>
2025-04-04 23:34:09 +0900

    [style] 모바일 화면에서도 같은 화면으로 보이게 하기 위한 반응형 CSS 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className='font-pretendard w-full'>{children}</body>
+      <body className='font-pretendard w-full bg-white'>{children}</body>
     </html>
   );
 }
